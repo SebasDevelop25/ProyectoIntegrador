725d77ce56c7ed25aa2ae1db81ca4c43c50f9321
Sebastian Ruiz <sebasdavidruiz@gmail.com>
2025-05-26 12:05:58 -0500

    Frontend Registro y log-in

--- src/main/java/com/usta/proyectointegrador/controllers/AccessController.java
+++ src/main/java/com/usta/proyectointegrador/controllers/AccessController.java
@@ -1,4 +1,23 @@
 package com.usta.proyectointegrador.controllers;
 
+import com.usta.proyectointegrador.entities.UsersEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AccessController {
+
+    @GetMapping("/seleccionarRol")
+    public String mostrarRoles(Model model) {
+        return "/registro/seleccionRol";
+    }
+    @GetMapping("/registro/registro")
+    public String mostrarFormularioRegistro(@RequestParam("rol") String rol, Model model) {
+        model.addAttribute("rol", rol);
+        model.addAttribute("usuario", new UsersEntity());
+        return "/registro/registro";
+    }
+
 }
